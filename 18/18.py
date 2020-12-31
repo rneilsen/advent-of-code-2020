54ba71c80f6e6c9d5900502126474c338b117bc4
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-01-01 02:24:14 +1100

    Implemented 18-2, included missing test case in test_input1

--- 18/18.py
+++ 18/18.py
@@ -31,7 +31,38 @@ def evaluate(line: List[str]) -> Tuple[int, List[str]]:
     return val
 
 
-def part1():
+def part1() -> int:
     return sum([evaluate(list(line.replace(' ', ''))) for line in lines])
 
+def part2() -> int:
+    # parse input to insert parens around all addition expressions
+    new_lines = []
+    for line in lines:
+        line = line.replace(' ', '')
+        next_plus = line.find('+')
+        while next_plus != -1:
+            left_edge = next_plus - 1
+            paren_depth = (1 if line[left_edge] == ')' else 0)
+            while paren_depth > 0:
+                left_edge -= 1
+                if line[left_edge] == ')':
+                    paren_depth += 1
+                elif line[left_edge] == '(':
+                    paren_depth -= 1
+            right_edge = next_plus + 1
+            paren_depth = (1 if line[right_edge] == '(' else 0)
+            while paren_depth > 0:
+                right_edge += 1
+                if line[right_edge] == '(':
+                    paren_depth += 1
+                elif line[right_edge] == ')':
+                    paren_depth -= 1
+            if left_edge != 0 or right_edge != len(line) - 1:
+                line = line[:left_edge] + '(' + line[left_edge:right_edge + 1] + ')' + line[right_edge + 1:]
+            next_plus = line.find('+', next_plus + 2)
+        new_lines.append(line)
+
+    return sum([evaluate(list(line.replace(' ', ''))) for line in new_lines])
+
 print("Part 1:", part1())
+print("Part 2:", part2())
